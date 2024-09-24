bc425dc709b0b4c36d18224af7283e3ffcba99ad
mtk177a <mtk177a@gmail.com>
2024-09-25 03:17:25 +0900

    `RecordsController`に`index`, `new`, `create`, `edit`, `update`, `destroy`アクションを追加

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,19 +1,44 @@
 class RecordsController < ApplicationController
+  def index
+    @records = current_user.records.order(date: :desc)
+  end
+
   def new
+    @record = Record.new
   end
 
   def create
+    @record = current_user.records.build(record_params)
+    if @record.save
+      redirect_to records_path, notice: "記録が作成されました。"
+    else
+      flash.now[:alert] = "記録に失敗しました。"
+      render :new
+    end
   end
 
   def edit
+    @record = current_user.records.find(params[:id])
   end
 
   def update
+    @record = current_user.records.find(params[:id])
+    if @record.update(record_params)
+      redirect_to records_path, notice: "記録が更新されました。"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @record = current_user.records.find(params[:id])
+    @record.destroy!
+    redirect_to records_path, notice: "記録が削除されました。"
   end
 
-  def index
+  private
+
+  def record_params
+    params.require(:record).permit(:title, :memo, :date)
   end
 end
