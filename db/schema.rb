16477d553faaf6fdf0c1ed3d7e7da431e1ce09d0
mtk177a <mtk177a@gmail.com>
2024-09-25 02:33:59 +0900

    マイグレーションの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_24_145424) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_24_173207) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "records", force: :cascade do |t|
+    t.string "title"
+    t.text "memo"
+    t.date "date"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_records_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -22,4 +32,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_24_145424) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "records", "users"
 end
