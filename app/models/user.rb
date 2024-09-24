19ab52e73ec6d9dba27a0713ac8267fa29b58fbc
mtk177a <mtk177a@gmail.com>
2024-09-25 00:07:29 +0900

    `User`モデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:password] }
 end
