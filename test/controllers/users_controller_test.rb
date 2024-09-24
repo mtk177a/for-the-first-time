d33d2e3187ec5d307baad6b6682b64f44023c4df
mtk177a <mtk177a@gmail.com>
2024-09-25 02:03:43 +0900

    テストの修正

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,12 +2,14 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    get signup_path
     assert_response :success
   end
 
-  test "should get create" do
-    get users_create_url
-    assert_response :success
+  test "should create user" do
+    post users_path, params: { user: { email: "test@example.com", password: "password", password_confirmation: "password" } }
+    assert_response :redirect  # 正常に作成されたらリダイレクト
+    follow_redirect!
+    assert_response :success  # リダイレクト後のページが正常に表示されるか確認
   end
 end
