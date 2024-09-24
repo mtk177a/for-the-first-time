e5e0a0908de34276a5bbe8e64a05ace27899d1a3
mtk177a <mtk177a@gmail.com>
2024-09-25 00:55:33 +0900

    `UserSessions#new`, `UserSessions#create`, `UserSessions#destroy`の実装

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,10 +1,20 @@
 class UserSessionsController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
   def new
   end
 
   def create
+    if @user = login(params[:email], params[:password])
+      redirect_to root_path, notice: 'ログインに成功しました。'
+    else
+      flash.now[:alert] = 'ログインに失敗しました。'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    logout
+    redirect_to login_path, notice: 'ログアウトしました。'
   end
 end
