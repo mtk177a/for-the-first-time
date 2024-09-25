b764c8864754f2cb9b8bce7a60ded647a7675705
mtk177a <mtk177a@gmail.com>
2024-09-26 00:26:10 +0900

    `PublicRecords#index`にログインなしでアクセスできるように修正

--- app/controllers/public_records_controller.rb
+++ app/controllers/public_records_controller.rb
@@ -1,4 +1,6 @@
 class PublicRecordsController < ApplicationController
+  skip_before_action :require_login, only: [ :index ]
+
   def index
     @public_records = Record.includes(:user).order(date: :desc)
   end
