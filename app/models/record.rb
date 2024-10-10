f79c3b373b069ac810327d3bd396194b8307822a
mtk177a <mtk177a@gmail.com>
2024-10-10 17:07:24 +0900

    `Record`モデルに`Like`モデルとのアソシエーションを追加

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,7 @@
 class Record < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :likers, through: :likes, source: :user
 
   validates :title, presence: true
   validates :date, presence: true
