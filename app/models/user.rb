9953a75c49b1275511a68b059a3cd879ba6eb027
mtk177a <mtk177a@gmail.com>
2024-10-10 17:07:02 +0900

    `User`モデルに`Like`モデルとのアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   has_many :records, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_records, through: :likes, source: :record
 
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:password] }
