e95bf0831939350bd368a3282cbd6096e8f614f5
mtk177a <mtk177a@gmail.com>
2024-10-10 18:42:19 +0900

    `Likes#create`, `Likes#destroy`の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,26 @@
 class LikesController < ApplicationController
+  before_action :set_record
+
   def create
+    current_user.likes.create!(record: @record)
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to record_path(@record) }
+    end
   end
 
   def destroy
+    like = current_user.likes.find_by(record: @record)
+    like&.destroy
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to record_path(@record) }
+    end
+  end
+
+  private
+
+  def set_record
+    @record = Record.find(params[:record_id])
   end
 end
