57831b5bde357a13e6c5dd338f55c24777d4dc2b
mtk177a <mtk177a@gmail.com>
2024-10-11 18:21:58 +0900

    テストコードの修正

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -1,13 +1,34 @@
 require "test_helper"
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get likes_create_url
+  # 必要なフィクスチャをロード
+  fixtures :users, :records, :likes
+
+  setup do
+    @user = users(:one)
+    @record = records(:one)
+    login_as(@user)
+  end
+
+  test "should create like" do
+    assert_difference("Like.count", 1) do
+      post record_like_path(@record), params: { record_id: @record.id }
+    end
+    assert_response :redirect # 成功後のリダイレクト確認
+    follow_redirect! # リダイレクト先の確認
+  end
+
+  test "should destroy like" do
+    like = @user.likes.create(record: @record) # テスト用の「いいね」を作成
+    assert_difference('Like.count', -1) do
+      delete record_like_path(@record), params: { id: like.id }, as: :turbo_stream
+    end
     assert_response :success
   end
 
-  test "should get destroy" do
-    get likes_destroy_url
-    assert_response :success
+  private
+
+  def login_as(user)
+    post login_path, params: { email: user.email, password: 'password' }
   end
 end
--- test/controllers/public_records_controller_test.rb
+++ test/controllers/public_records_controller_test.rb
@@ -1,8 +1,22 @@
 require "test_helper"
 
 class PublicRecordsControllerTest < ActionDispatch::IntegrationTest
+  # 必要なフィクスチャをロード
+  fixtures :users, :records, :likes
+
+  setup do
+    @user = users(:one)
+    login_as(@user) # ログイン処理を追加
+  end
+
   test "should get index" do
     get public_records_path
     assert_response :success
+  end
+
+  private
+
+  def login_as(user)
+    post login_path, params: { email: user.email, password: 'password' } # 適切なパスワードを設定
   end
 end
